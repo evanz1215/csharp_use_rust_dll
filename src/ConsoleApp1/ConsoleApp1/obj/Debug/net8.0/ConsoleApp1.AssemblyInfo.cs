c99c1b8c2eb9215c1574784f73eb2103eed14e6a
Evan <evanz1215@gmail.com>
2024-02-04 01:00:04 +0800

    Update assembly version in ConsoleApp1.AssemblyInfo.cs

--- src/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ src/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22342055744133ecbb7751efd8f1a58597f209de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a76aaacc63e6fd73b98b713c0ad07303a097ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
